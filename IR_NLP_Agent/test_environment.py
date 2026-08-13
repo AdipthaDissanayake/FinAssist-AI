2f188a462bc71dc289f102ee420f1af49656da48
Adiptha <adipthathisayuru0529@gmail.com>
2026-08-13 22:54:03 +0530

    Dependancies and libraries downloaded

---
+++ IR_NLP_Agent/test_environment.py
@@ -0,0 +1,10 @@
+import pypdf
+import pandas
+import sklearn
+import spacy
+
+print("Environment is working!")
+print("pypdf:", pypdf.__version__)
+print("pandas:", pandas.__version__)
+print("scikit-learn:", sklearn.__version__)
+print("spaCy:", spacy.__version__)
